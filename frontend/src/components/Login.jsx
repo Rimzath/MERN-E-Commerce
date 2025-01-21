3a9cbf3eed8db38b163edee69fa0811f83dfd02d
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-21 11:02:21 +0530

    created handle submit for frontend post method

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,10 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import { Link } from "react-router-dom";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        axios.post( 'http://localhost:3001/login', {email, password})
+        .then(result => {
+            console.log(result);
+            if(result.data === "Success"){
+                console.log("Login Success");
+                alert('Login successful!')
+                navigate('/home');
+            }
+            else{
+                alert('Incorrect password! Please try again.');
+            }
+        })
+        .catch(err => console.log(err));
+    }
 
 
     return (
@@ -12,7 +33,7 @@ const Login = () => {
             <div className="d-flex justify-content-center align-items-center text-center vh-100" style= {{backgroundImage : "linear-gradient(#00d5ff,#0095ff,rgba(93,0,255,.555))"}}>
                 <div className="bg-white p-3 rounded" style={{width : '40%'}}>
                     <h2 className='mb-3 text-primary'>Login</h2>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-3 text-start">
                             <label htmlFor="exampleInputEmail1" className="form-label">
                                 <strong>Email Id</strong>
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,12 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 import { Link } from "react-router-dom";
-
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Register = () => {
     const [name, setName] = useState();
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
+    const navigate = useNavigate();
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+        axios.post( 'http://localhost:3001/register', {name, email, password})
+        .then(result => {
+            console.log(result);
+            if(result.data === "Already registered"){
+                alert("E-mail already registered! Please Login to proceed.");
+                navigate('/login');
+            }
+            else{
+                alert("Registered successfully! Please Login to proceed.")
+                navigate('/login');
+            }
+            
+        })
+        .catch(err => console.log(err));
+    }
 
 
     return (
@@ -14,7 +35,7 @@ const Register = () => {
             <div className="d-flex justify-content-center align-items-center text-center vh-100" style= {{backgroundImage : "linear-gradient(#00d5ff,#0095ff,rgba(93,0,255,.555))"}}>
                 <div className="bg-white p-3 rounded" style={{width : '40%'}}>
                     <h2 className='mb-3 text-primary'>Register</h2>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <div className="mb-3 text-start">
                             <label htmlFor="exampleInputEmail1" className="form-label">
                                 <strong >Name</strong>
