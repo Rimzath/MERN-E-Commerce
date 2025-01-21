1152a7165cc6f19d797cb363f58f2af5894b7511
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-21 10:56:31 +0530

    added routes to app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import Register from './components/Register.jsx';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from './components/Login';
+import Home from './components/Home';
 
 function App() {
   
   return (
     <BrowserRouter >
         <Routes>
-          <Route path="/" element ={<Register/>} />
+           <Route path="/" element ={<Register/>} />
+          <Route path="/register" element ={<Register/>} />
           <Route path="/login" element ={<Login/>} />
+          <Route path="/home" element ={<Home/>} />
         </Routes>
       </BrowserRouter>
   )
