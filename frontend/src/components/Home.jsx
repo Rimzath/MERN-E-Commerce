5f655fadfd3079d83b8ed80d5c588ec0099b88f2
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-21 12:15:45 +0530

    changed UI

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div
       style={{
-        backgroundImage: "linear-gradient(#00d5ff,#0095ff,rgba(93,0,255,.555))",
+        backgroundImage: "linear-gradient(#00e3e6,#14d1e8,#6686f0,#707df1,#816df3,#9759f5,#ae44f7,#f800ff)",
       }}
       className="d-flex flex-column justify-content-center align-items-center text-center vh-100"
     >
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -30,7 +30,7 @@ const Login = () => {
 
     return (
         <div>
-            <div className="d-flex justify-content-center align-items-center text-center vh-100" style= {{backgroundImage : "linear-gradient(#00d5ff,#0095ff,rgba(93,0,255,.555))"}}>
+            <div className="d-flex justify-content-center align-items-center text-center vh-100" style= {{backgroundImage : "linear-gradient(#00e3e6,#14d1e8,#6686f0,#707df1,#816df3,#9759f5,#ae44f7,#f800ff)"}}>
                 <div className="bg-white p-3 rounded" style={{width : '40%'}}>
                     <h2 className='mb-3 text-primary'>Login</h2>
                     <form onSubmit={handleSubmit}>
