43145aa503578d939516e7626580d79e5e22208b
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-23 19:26:22 +0530

    created users form

--- frontend/src/components/CRUD/Users.jsx
+++ frontend/src/components/CRUD/Users.jsx
@@ -1,7 +1,66 @@
 import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const users = () => {
-  return <div>Welcome users</div>;
+  const [users, setUsers] = useState([]);
+
+  return (
+    <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
+      <div className="tab vw-100 w-50 bg-white rounded p-3">
+        <div>
+          {" "}
+          <h2>Home Page</h2>
+        </div>
+
+        {/*  Botão que chama a tela ou rota criar usuário, usando um link */}
+        <Link to="/create" className="btn btn-success">
+          Add +
+        </Link>
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Age</th>
+              <th colSpan="2">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              console.log(user);
+              return (
+                // eslint-disable-next-line react/jsx-key
+                <tr key={user._id}>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>{user.age}</td>
+                  <td>
+                    <Link
+                      to={`/update/${user._id}`}
+                      className="btnEdit btn btn-success"
+                    >
+                      Update
+                    </Link>
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger"
+                      id="btnDelete"
+                      onClick={() => handleDelete(user._id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default users;
