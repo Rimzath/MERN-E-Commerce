9fab83a1d472093092f9875618b0837ecb40e923
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-23 19:16:20 +0530

    created crud operation jsx files and initialization it

---
+++ frontend/src/components/CRUD/CreateUser.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Createuser = () => {
+  return <div>Createuser</div>;
+};
+
+export default Createuser;
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,14 +1,16 @@
 // import React from 'react';
 import { Link } from "react-router-dom";
-
+import Users from "./CRUD/users";
 const Home = () => {
   return (
     <div
       style={{
-        backgroundImage: "linear-gradient(#00e3e6,#14d1e8,#6686f0,#707df1,#816df3,#9759f5,#ae44f7,#f800ff)",
+        backgroundImage:
+          "linear-gradient(#00e3e6,#14d1e8,#6686f0,#707df1,#816df3,#9759f5,#ae44f7,#f800ff)",
       }}
       className="d-flex flex-column justify-content-center align-items-center text-center vh-100"
     >
+      <Users />
       <h1>Login Successfully</h1>
       <Link to="/login" className="btn btn-light my-5">
         Logout
