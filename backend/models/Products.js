8124b2fbd84ec9b5b731ba6c5cb79eae2ecba1e4
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-23 21:13:20 +0530

    created collection for products

---
+++ backend/models/Products.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  quantity: Number,
+});
+
+const ProductModel = mongoose.model("products", ProductSchema);
+
+module.exports = ProductModel;
