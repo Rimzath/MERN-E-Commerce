673df0705c2d29b566e385462890ee7e2c740021
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-25 21:37:23 +0530

    changed bg color

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,13 +5,12 @@ const Home = () => {
   return (
     <div
       style={{
-        backgroundImage:
-          "linear-gradient(#00e3e6,#14d1e8,#6686f0,#707df1,#816df3,#9759f5,#ae44f7,#f800ff)",
+        backgroundColor: "#276EFC",
       }}
       className="d-flex flex-column justify-content-center align-items-center text-center vh-100"
     >
       <Products />
-      <h1>Login Successfully</h1>
+      <h5>Login Successfully</h5>
       <Link to="/login" className="btn btn-light my-5">
         Logout
       </Link>
