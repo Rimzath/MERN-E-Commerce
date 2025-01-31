f611ae667584db1fefb41b279826c36cdd6d7040
Rimzath Ahamed <129421801+Rimzath@users.noreply.github.com>
2025-01-31 10:52:53 +0530

    changed the path accoding to good ER diagram

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Register />} />
+        <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/home">
+              <Link className="nav-link" to="/login">
                 Products
               </Link>
             </li>
